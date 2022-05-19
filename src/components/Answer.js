edc13baaba24d0bd7071d05ec6bbfda0b16456e4
aitor <penguinft1@gmail.com>
2022-05-19 16:14:27 +0200

    First commit

---
+++ src/components/Answer.js
@@ -0,0 +1,30 @@
+import React from "react"
+
+export default function Answer(props) {
+    let statusClass = props.selected ? "answer--checked" : ""
+    
+    if (props.validate) {
+        if (props.selected) {
+            if (props.correct) {
+                statusClass = "answer--correct"
+            } else {
+                statusClass = "answer--wrong"
+            }
+        } else {
+            if (props.correct) {
+                statusClass = "answer--correct"
+            } else {                
+                statusClass = "answer--after-check"
+            }
+        }
+    }
+    
+    return (
+        <span 
+            className={`answer ${statusClass}`} 
+            onClick={props.selectAnswer}           
+        >
+            {props.answer}
+        </span>
+    )
+}
